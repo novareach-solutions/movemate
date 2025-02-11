7c6b793ed6ebf7a6a7342fe19e4c8780a730921f
abdul-ahad12 <abdulkouzer@gmail.com>
2025-02-11 20:46:23 +0530

    fix: Import Image on earngin details screen

--- apps/frontend/mobile/Rider/src/Screens/EarningDetailsScreen.tsx
+++ apps/frontend/mobile/Rider/src/Screens/EarningDetailsScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import EarningsModal from '../components/Modals/EarningsModal';
 import { colors } from '../theme/colors';
 import { useNavigation } from '@react-navigation/native';
